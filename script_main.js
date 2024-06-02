7ab289fedd7aac3c6fa4cf9ae3e431b532388f3b
Putra Jadi Mukti <putramukti26@gmail.com>
2024-06-03 05:41:02 +0700

    perbaikan

--- script_main.js
+++ script_main.js
@@ -16,7 +16,7 @@ window.addEventListener("DOMContentLoaded", (ev)=>{
       return; 
     }
 
-    document.querySelector(".nama").innerText = localStorage.getItem("nama");
+    document.querySelector("#nama").innerText = localStorage.getItem("nama");
 
     setGenres();
     
